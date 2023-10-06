b36d9214687fc3cf361a973cea97496d8166495e
FoggDev <foggdev@gmail.com>
2023-10-05 23:33:43 -0600

    fix: Moving config functions to devtools

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,38 +1,7 @@
-import fs from 'fs'
 import path from 'path'
+import { loadUserConfig, mergeConfig } from '@architecturex/devtools'
 
 import defaultConfig, { Configuration } from './defaultConfig'
-
-function loadUserConfig(configPath: string): object {
-  if (!fs.existsSync(configPath)) {
-    return {};
-  }
-
-  const fileContents = fs.readFileSync(configPath, 'utf8');
-
-  try {
-    return JSON.parse(fileContents);
-  } catch (err) {
-    console.error(err);
-    return {};
-  }
-}
-
-function deepMerge(target: any, source: any): object {
-  for (const key in source) {
-    if (source[key] instanceof Object && target[key]) {
-      target[key] = deepMerge(target[key], source[key])
-    } else {
-      target[key] = source[key]
-    }
-  }
-
-  return target
-}
-
-function mergeConfig(defaults: object, userConfig: object): object {
-  return deepMerge({ ...defaults }, userConfig)
-}
 
 const configPath = process.cwd() + '/.architecturexrc.json'
 const userConfigPath = path.resolve(configPath)
