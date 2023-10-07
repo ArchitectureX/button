8f752546cd3662d790b971bfc3c6856cacdaf320
FoggDev <foggdev@gmail.com>
2023-10-07 00:30:10 -0600

    fix: Moving loadUserConfig to utils.config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,18 +1,7 @@
-import fs from 'fs'
 import path from 'path'
-import { mergeConfig } from '@architecturex/utils.config'
+import { mergeConfig, loadUserConfig } from '@architecturex/utils.config'
 
 import defaultConfig, { Configuration } from './defaultConfig'
-
-function loadUserConfig(configPath: string): object {
-  if (!fs.existsSync(configPath)) {
-    return {}
-  }
-
-  const fileContents = fs.readFileSync(configPath, 'utf8')
-  console.log('FILE CONTENTS', fileContents)
-  return JSON.parse(fileContents)
-}
 
 const configPath = process.cwd() + '/.architecturexrc.json'
 const userConfigPath = path.resolve(configPath)
